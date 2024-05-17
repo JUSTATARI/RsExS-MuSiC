98770091715990f3b514af5b8d92f5a96e212209
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-05-17 23:41:53 +0300

    D2

--- YukkiMusic/plugins/admins/seek.py
+++ YukkiMusic/plugins/admins/seek.py
@@ -1,9 +1,9 @@
 #
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/TeamYukki/RsExSMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/TeamYukki/RsExSMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import YouTube, app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.misc import db
-from YukkiMusic.utils import AdminRightsCheck, seconds_to_min
+from RsExSMusic import YouTube, app
+from RsExSMusic.core.call import Yukki
+from RsExSMusic.misc import db
+from RsExSMusic.utils import AdminRightsCheck, seconds_to_min
 
 # Commands
 SEEK_COMMAND = get_command("SEEK_COMMAND")
