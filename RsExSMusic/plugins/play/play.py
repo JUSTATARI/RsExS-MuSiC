62ad6706436b4903ff1e41fae67b437ee4c83eeb
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-05-18 14:28:45 +0300

    Update play.py

--- RsExSMusic/plugins/play/play.py
+++ RsExSMusic/plugins/play/play.py
@@ -21,21 +21,21 @@ from config import (BANNED_USERS, lyrical, YAFA_NAME,
                     YAFA_CHANNEL, CHANNEL_SUDO)
 from strings import get_command
 from strings.filters import command
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from RsExSMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils import seconds_to_min, time_to_seconds
-from YukkiMusic.utils.channelplay import get_channeplayCB
-from YukkiMusic.utils.database import is_video_allowed
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.decorators.play import PlayWrapper
-from YukkiMusic.utils.formatters import formats
-from YukkiMusic.utils.inline.play import (livestream_markup,
+from RsExSMusic.core.call import Yukki
+from RsExSMusic.utils import seconds_to_min, time_to_seconds
+from RsExSMusic.utils.channelplay import get_channeplayCB
+from RsExSMusic.utils.database import is_video_allowed
+from RsExSMusic.utils.decorators.language import languageCB
+from RsExSMusic.utils.decorators.play import PlayWrapper
+from RsExSMusic.utils.formatters import formats
+from RsExSMusic.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from YukkiMusic.utils.inline.playlist import botplaylist_markup
-from YukkiMusic.utils.logger import play_logs
-from YukkiMusic.utils.stream.stream import stream
+from RsExSMusic.utils.inline.playlist import botplaylist_markup
+from RsExSMusic.utils.logger import play_logs
+from RsExSMusic.utils.stream.stream import stream
 
 
 force_btn = InlineKeyboardMarkup(
