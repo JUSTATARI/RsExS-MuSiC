8d531a0fa993cce50828a6d532e5f70cfaa6b7b3
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-05-18 14:30:19 +0300

    Update toptracks.py

--- RsExSMusic/plugins/play/toptracks.py
+++ RsExSMusic/plugins/play/toptracks.py
@@ -13,14 +13,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
 
 from config import BANNED_USERS
-from YukkiMusic import app
-from YukkiMusic.utils.database import (get_global_tops,
+from RsExSMusic import app
+from RsExSMusic.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
+from RsExSMusic.utils.decorators.language import languageCB
+from RsExSMusic.utils.inline.playlist import (botplaylist_markup,
                                               failed_top_markup,
                                               top_play_markup)
-from YukkiMusic.utils.stream.stream import stream
+from RsExSMusic.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
 
