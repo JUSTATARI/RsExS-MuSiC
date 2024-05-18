27d935aa6d20c29ef78ec299b2191a76a0f201e2
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-05-18 14:32:22 +0300

    Update heroku.py

--- RsExSMusic/plugins/sudo/heroku.py
+++ RsExSMusic/plugins/sudo/heroku.py
@@ -24,13 +24,13 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import HAPP, SUDOERS, XCB
-from YukkiMusic.utils.database import (get_active_chats,
+from RsExSMusic import app
+from RsExSMusic.misc import HAPP, SUDOERS, XCB
+from RsExSMusic.utils.database import (get_active_chats,
                                        remove_active_chat,
                                        remove_active_video_chat)
-from YukkiMusic.utils.decorators.language import language
-from YukkiMusic.utils.pastebin import Yukkibin
+from RsExSMusic.utils.decorators.language import language
+from RsExSMusic.utils.pastebin import Yukkibin
 
 # Commands
 GETLOG_COMMAND = get_command("GETLOG_COMMAND")
