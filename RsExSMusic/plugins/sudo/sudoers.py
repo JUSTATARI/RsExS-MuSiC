d38553853a61c9164f5f4b83168fa35b4aab3634
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-05-18 14:33:48 +0300

    Update sudoers.py

--- RsExSMusic/plugins/sudo/sudoers.py
+++ RsExSMusic/plugins/sudo/sudoers.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MONGO_DB_URI, OWNER_ID
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_sudo, remove_sudo
-from YukkiMusic.utils.decorators.language import language
+from RsExSMusic import app
+from RsExSMusic.misc import SUDOERS
+from RsExSMusic.utils.database import add_sudo, remove_sudo
+from RsExSMusic.utils.decorators.language import language
 
 # Command
 ADDSUDO_COMMAND = get_command("ADDSUDO_COMMAND")
