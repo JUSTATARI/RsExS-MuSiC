8870502da439db75973b8f67870be7d71a06091c
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-05-18 14:09:07 +0300

    Update pause.py

--- RsExSMusic/plugins/admins/pause.py
+++ RsExSMusic/plugins/admins/pause.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_music_playing, music_off
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from RsExSMusic import app
+from RsExSMusic.core.call import Yukki
+from RsExSMusic.utils.database import is_music_playing, music_off
+from RsExSMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 PAUSE_COMMAND = get_command("PAUSE_COMMAND")
