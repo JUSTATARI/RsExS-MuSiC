cea899a9c0e4f47a00b5813edc10fc65a5182ea3
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-05-18 14:28:13 +0300

    Update live.py

--- RsExSMusic/plugins/play/live.py
+++ RsExSMusic/plugins/play/live.py
@@ -10,10 +10,10 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from YukkiMusic import YouTube, app
-from YukkiMusic.utils.channelplay import get_channeplayCB
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.stream.stream import stream
+from RsExSMusic import YouTube, app
+from RsExSMusic.utils.channelplay import get_channeplayCB
+from RsExSMusic.utils.decorators.language import languageCB
+from RsExSMusic.utils.stream.stream import stream
 
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
